b26c173b4114eecf8813935206e75d756d5c36d5
Weikang01 <lwkdota@qq.com>
2021-06-27 18:44:58 -0400

    After Level Design and Click to Move

--- Assets/Scripts/WorldInteraction.cs
+++ Assets/Scripts/WorldInteraction.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class WorldInteraction : MonoBehaviour
 {
+    NavMeshAgent playerAgent;
+
+    private void Start()
+    {
+        playerAgent = GetComponent<NavMeshAgent>();
+    }
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
@@ -12,6 +20,19 @@ public class WorldInteraction : MonoBehaviour
 
     void GetInteraction()
     {
-
+        Ray interactionRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit interactionInfo;
+        if (Physics.Raycast(interactionRay, out interactionInfo, Mathf.Infinity))
+        {
+            GameObject interactedObject = interactionInfo.collider.gameObject;
+            if (interactedObject.tag == "Interactable Object")
+            {
+                Debug.Log("Interactable interacted.");
+            }
+            else
+            {
+                playerAgent.destination = interactionInfo.point;
+            }
+        }
     }
 }
