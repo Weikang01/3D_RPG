a989dffac40abfb692f21e2c95d070c961f5a5ad
Weikang01 <lwkdota@qq.com>
2021-07-03 02:35:04 -0400

    After Quest

---
+++ Assets/Scripts/Questing/Quest.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Quest
+{
+    public List<Goal> Goals { get; set; } = new List<Goal>();
+    public string QuestName { get; set; }
+    public string Description { get; set; }
+    public int ExperienceReward { get; set; }
+    public Item ItemReward { get; set; }
+    public bool Completed { get; set; }
+
+    public void CheckGoals()
+    {
+        Completed = Goals.All(g => g.Completed);
+        if (Completed) GiveReward();
+    }
+
+    void GiveReward()
+    {
+        if (ItemReward != null)
+        {
+            InventoryController.instance.GiveItem(ItemReward);
+        }
+    }
+}
